43eb108752812c47d4350448fb2578e7089dbe2a
Oleksandr Markov <ol.markov1999@gmail.com>
2021-02-16 21:44:17 +0200

    + partners block and adaptive

--- js/main.js
+++ js/main.js
@@ -5,9 +5,27 @@ $('.reviews-slider').slick({
   slidesToShow: 2,
   slidesToScroll: 2,
   arrows: false,
-  dots: true
+  dots: true,
+
+/*responsive - from slider slick website*/
+  responsive: [
+    {
+    	/*if screen width = 1020 - to break slider:
+    	1 slide in 1 row*/
+      breakpoint: 1020,
+      settings: {
+        slidesToShow: 1,
+  		slidesToScroll: 1,
+      }
+    },
+]
+
 });
 
+
+$('.btn-menu').on('click', function(){
+$('.menu__list').slideToggle();
+});
 
 
 
